2c37345013dcaeed18060cd1a657ae9875e7982b
Kyle Walters <kyle.p.walters@gmail.com>
2020-07-30 20:40:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,6 @@ end
 
 def list_dwarves(array)
   array.each_with_index do | single_dwarf, dwarfs_index_in_array |
-    puts "dwarfs_index_in_array: #{dwarfs_index_in_array} for #{single_dwarf}"
+    puts "dwarfs_index_in_array: #{single_dwarf for #{dwarfs_index_in_array}"
   end
 end
